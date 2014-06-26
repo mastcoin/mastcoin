71141a9b96d14a124e2153e07a9f319f7a38abe8
MastiffCoin <mastiffcoin@gmail.com>
2014-06-26 19:46:49 -0400

    Keep datadir as .MastiffCoin and .conf file MastiffCoin.conf to retain backwards compatibility.

--- src/util.cpp
+++ src/util.cpp
@@ -1066,7 +1066,7 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
 
 boost::filesystem::path GetConfigFile()
 {
-    boost::filesystem::path pathConfigFile(GetArg("-conf", "MastCoin.conf"));
+    boost::filesystem::path pathConfigFile(GetArg("-conf", "MastiffCoin.conf"));
     if (!pathConfigFile.is_complete()) pathConfigFile = GetDataDir(false) / pathConfigFile;
     return pathConfigFile;
 }
